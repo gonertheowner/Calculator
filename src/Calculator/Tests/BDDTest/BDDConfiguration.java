e6d217d015a1cbe203f4ae5cd0ddac770cd41693
Kirill Babich <gonertheowner@gmail.com>
2022-10-27 21:57:54 +0300

    Set the BDD configuration

--- src/Calculator/Tests/BDDTest/BDDConfiguration.java
+++ src/Calculator/Tests/BDDTest/BDDConfiguration.java
@@ -1,4 +1,37 @@
 package Calculator.Tests.BDDTest;
 
-public class BDDConfiguration {
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.io.LoadFromClasspath;
+import org.jbehave.core.io.StoryFinder;
+import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
+
+import java.util.List;
+
+import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
+import static org.jbehave.core.reporters.Format.CONSOLE;
+
+public class BDDConfiguration extends JUnitStories {
+    @Override
+    public Configuration configuration() {
+        return new MostUsefulConfiguration()
+                .useStoryLoader(new LoadFromClasspath(this.getClass()))
+                .useStoryReporterBuilder(new StoryReporterBuilder()
+                        .withCodeLocation(codeLocationFromClass(this.getClass()))
+                        .withFormats(CONSOLE));
+    }
+
+    @Override
+    public InjectableStepsFactory stepsFactory() {
+        return new InstanceStepsFactory(configuration(), new SummaryBDDTest());
+    }
+
+    @Override
+    public List<String> storyPaths() {
+        StoryFinder finder = new StoryFinder();
+        return List.of("TestStory.story");
+    }
 }
